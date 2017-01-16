b8d84ee2180a03afb8a9cb11d73b418eb0648fea
Max Mirren <Max Mirren>
2017-01-16 16:22:19 +0300

    Initial commit

---
+++ app/src/main/java/com/testem/maxm/testem/AuthActivity.java
@@ -0,0 +1,13 @@
+package com.testem.maxm.testem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth_activity);
+    }
+}
